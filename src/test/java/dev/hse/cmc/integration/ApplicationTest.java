acca276fb223747c7deb5fcbce125beeea8d4d50
ArchieSW <archie-woc@ya.ru>
2023-03-12 13:26:24 +0300

    implement integration test for ProcessingService

--- src/test/java/dev/hse/cmc/integration/ApplicationTest.java
+++ src/test/java/dev/hse/cmc/integration/ApplicationTest.java
@@ -1,17 +1,25 @@
 package dev.hse.cmc.integration;
 
+import dev.hse.cmc.common.TestConstants;
 import dev.hse.cmc.integration.common.AbstractIntegrationTest;
+import dev.hse.cmc.kafka.KafkaConsumerService;
 import dev.hse.cmc.kafka.KafkaProducerService;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 class ApplicationTest extends AbstractIntegrationTest {
 
+    @MockBean
+    private KafkaProducerService producerService;
     @Autowired
-    private KafkaProducerService userKafkaProducer;
+    private KafkaConsumerService consumerService;
 
     @Test
     void testProduceAndConsumeKafkaMessage() {
+        consumerService.consume(TestConstants.TEST_RAW_DATA);
+        Mockito.verify(producerService).send(TestConstants.FILTERED_TEST_DATA);
     }
 }
 
